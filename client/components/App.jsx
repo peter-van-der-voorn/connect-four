dc2773edda116e6d5ca62267579716c4a22a2597
Peter van der Voorn <peteranthonius@gmail.com>
2021-06-17 13:59:00 +1200

    add css grid

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Header from './Header'
+import Board from './Board'
+
+function App () {
+  return (
+    <>
+      <Header />
+      <Board />
+    </>
+
+  )
+}
+
+export default App
---
+++ client/components/Board.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+function Board () {
+  return (
+    <div className="grid-container">
+      <div className="grid-item a1"></div>
+      <div className="grid-item a2"></div>
+      <div className="grid-item a3"></div>
+      <div className="grid-item a4"></div>
+      <div className="grid-item a5"></div>
+      <div className="grid-item a6"></div>
+      <div className="grid-item a7"></div>
+
+      <div className="grid-item b1"></div>
+      <div className="grid-item b2"></div>
+      <div className="grid-item b3"></div>
+      <div className="grid-item b4"></div>
+      <div className="grid-item b5"></div>
+      <div className="grid-item b6"></div>
+      <div className="grid-item b7"></div>
+
+      <div className="grid-item c1"></div>
+      <div className="grid-item c2"></div>
+      <div className="grid-item c3"></div>
+      <div className="grid-item c4"></div>
+      <div className="grid-item c5"></div>
+      <div className="grid-item c6"></div>
+      <div className="grid-item c7"></div>
+
+      <div className="grid-item d1"></div>
+      <div className="grid-item d2"></div>
+      <div className="grid-item d3"></div>
+      <div className="grid-item d4"></div>
+      <div className="grid-item d5"></div>
+      <div className="grid-item d6"></div>
+      <div className="grid-item d7"></div>
+
+      <div className="grid-item e1"></div>
+      <div className="grid-item e2"></div>
+      <div className="grid-item e3"></div>
+      <div className="grid-item e4"></div>
+      <div className="grid-item e5"></div>
+      <div className="grid-item e6"></div>
+      <div className="grid-item e7"></div>
+
+      <div className="grid-item f1"></div>
+      <div className="grid-item f2"></div>
+      <div className="grid-item f3"></div>
+      <div className="grid-item f4"></div>
+      <div className="grid-item f5"></div>
+      <div className="grid-item f6"></div>
+      <div className="grid-item f7"></div>
+
+    </div>
+  )
+}
+
+export default Board
