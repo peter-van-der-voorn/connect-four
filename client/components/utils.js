d6f8550495b259ba1f2c8a246486c272c2264ebd
Peter van der Voorn <peteranthonius@gmail.com>
2021-06-25 19:56:16 +1200

    tidy up before branching

--- client/components/utils.js
+++ client/components/utils.js
@@ -1,3 +1,13 @@
+import useSound from 'use-sound'
+
+//
+//  LET THERE BE SFX!
+//
+
+// import token1Sfx from '../../server/public/audio/token-1.mp3'
+
+// const [playToken1] = useSound(token1Sfx)
+
 // Returns the lowest available cell. If column is full, returns -1
 export function findLowestCell (col, boardState) {
   for (let row = 0; row < boardState.length; row++) {
@@ -20,6 +30,7 @@ export function addToken (col, colour, setBoardState, boardState) {
     const board = [...prevBoardState]
     board[row] = [...board[row]]
     board[row][col] = colour
+    // playToken1()
     return board
   })
 }
